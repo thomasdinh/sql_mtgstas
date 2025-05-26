3016ea25a75dda2663a900034cc71839de9ad245
thomasdinh <thomas.dinh.work@outlook.de>
2025-05-26 18:20:59 +0200

    added functions for decklose

--- databaseinsql.py
+++ databaseinsql.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import datetime
 from mysql.connector import Error
 
 def connect_to_database():
@@ -19,6 +20,9 @@ def connect_to_database():
     except Error as e:
         print(f"Error: {e}")
         return None
+
+def get_current_date():
+    return datetime.today().strftime('%d.%m.%y')
 
 def execute_query(connection):
     try:
@@ -149,7 +153,7 @@ def deckarray_to_deck_id_array(decklist_array):
     print(deck_ids)
     return deck_ids
 
-def delete_max_matchid_entry(table_name):
+def delete_max_matchid_entry_deckwin():
     connection = None
     try:
         # Connect to the database
@@ -158,13 +162,27 @@ def delete_max_matchid_entry(table_name):
 
         # SQL query to delete the entry with the maximum matchid
         query = f"""
-            DELETE FROM {table_name}
+            DELETE FROM deckwin
             WHERE matchid = (
                 SELECT max_matchid FROM (
-                    SELECT MAX(matchid) AS max_matchid FROM {table_name}
+                    SELECT MAX(matchid) AS max_matchid FROM deckwin
                 ) AS temp
             );
         """
+
+        """ Error: 1093 (HY000): You can't specify target table 'deckwin' for update in FROM clause
+        occurs because MySQL does not allow you to update or delete rows from a table and select from the same table in a subquery within the same SQL statement.
+        Solutions to the Problem
+
+        There are a few ways to work around this limitation:
+
+        Use a Temporary Table:
+            Store the result of the subquery in a temporary table and then use this temporary table in your main query.
+
+        Use a Derived Table:
+            Use a derived table (a subquery in the FROM clause) to achieve the same result. """
+
+        
 
         # Execute the query
         cursor.execute(query)
@@ -208,7 +226,7 @@ def add_mtgmatches_entry(decklists, winnerID, date=None, matchID=None):
             data.append(pos_match_id + 1)  # Increment to get the next ID
         data.append(decklists)
         if date is None:
-            current_date = "25-05-2025"  # Use the correct date format for your database
+            current_date = get_current_date()  # Use the correct date format for your database
         else:
             current_date = date
         data.append(current_date)
@@ -233,6 +251,33 @@ def add_mtgmatches_entry(decklists, winnerID, date=None, matchID=None):
         if connection is not None and connection.is_connected():
             connection.close()
             print("MySQL connection is closed")
+
+def del_mtgmatches_entry(matchID= None):
+    to_del_entry = matchID
+    connection = None
+    try:
+        # Connect to the database
+        connection = connect_to_database()
+        cursor = connection.cursor()
+
+        # Query to get the maximum entry count
+        max_entry_count_query = "SELECT COUNT(*) FROM mtgmatches"
+        cursor.execute(max_entry_count_query)
+        max_entry_result = cursor.fetchone()
+        pos_match_id = max_entry_result[0]
+        if to_del_entry == None:
+            to_del_entry = pos_match_id
+        
+        del_mtg_match_query = "DELETE FROM mtgmatches WHERE matchID = %s"
+        cursor.execute(del_mtg_match_query, (to_del_entry,))
+        connection.commit()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        if connection is not None:
+            connection.rollback()  # Rollback in case of error
+
+    
 
 def add_deck_win_entry(matchID, deckID, deckopponentID, result, date = None, ):
     try:
@@ -267,11 +312,84 @@ def add_deck_win_entry(matchID, deckID, deckopponentID, result, date = None, ):
         if connection is not None and connection.is_connected():
             connection.close()
             print("MySQL connection is closed")
-        
+
+# Result 0 - Lose, 1 - Draw
+def add_deck_lose(matchID, deckID, OpponenDeckID, Result, date = None):
+    current_date = None
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor()
+
+        query = "INSERT Into decklose (matchid, deckid, opponentDeckID,result, date) \n" \
+            "VALUES (%s, %s, %s, %s, %s);"
+    
+        if date == None:
+            current_date = get_current_date()
+        else:
+            current_date = date
+        data = []
+        data.append(matchID)
+        data.append(deckID)
+        data.append(OpponenDeckID)
+        data.append(Result)
+        data.append(current_date)
+        print(f'Add to decklose : {data}')
+        cursor.execute(query, tuple(data))
+        connection.commit()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        if connection is not None:
+            connection.rollback()  # Rollback in case of error
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None and connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
+
+def delete_deck_lose(matchID=None):
+    connection = None
+    cursor = None
+    try:
+        connection = connect_to_database()
+        cursor = connection.cursor()
+
+        if matchID is not None:
+            query = "DELETE FROM decklose WHERE matchid = %s;"
+            cursor.execute(query, (matchID,))
+        else:
+            # Find the maximum matchID
+            query_max_id = "SELECT MAX(matchid) FROM decklose;"
+            cursor.execute(query_max_id)
+            max_id = cursor.fetchone()[0]
+
+            if max_id is not None:
+                query = "DELETE FROM decklose WHERE matchid = %s;"
+                cursor.execute(query, (max_id,))
+                print(f"Deleted entry with max matchID: {max_id}")
+            else:
+                print("No entries found in the table.")
+
+        connection.commit()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        if connection is not None:
+            connection.rollback()  # Rollback in case of error
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None and connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
+     
 
 if __name__ == "__main__":
     connection = connect_to_database()
-    test_array = convert_decklist_to_array('Ghired, Obeka 2, Obeka, Bethor')
-    #add_deck_win_entry(matchID = 1, deckID = 1, deckopponentID =2, result= 1, date = None)
-    delete_max_matchid_entry('deckwin')
+    delete_deck_lose()
+    
+    
 
--- test_cases.py
+++ test_cases.py
@@ -3,4 +3,4 @@ import databaseinsql
 if __name__ == "__main__":
     connection = databaseinsql.connect_to_database()
     test_array = databaseinsql.convert_decklist_to_array('Ghired, Obeka 2, Obeka, Bethor')
-    #databaseinsql.add_mtgmatches_entry(winnerID=1, decklists='1,2,3,4')
+    databaseinsql.add_mtgmatches_entry(winnerID=1, decklists='1,2,3,4')
